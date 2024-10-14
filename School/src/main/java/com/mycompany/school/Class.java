9dcfe6acf0dce6b980db51b6d30c39466553caac
ShiDaAl <158536962+ShiDaAl@users.noreply.github.com>
2024-10-14 14:27:22 +0900

    Add files via upload

---
+++ School/src/main/java/com/mycompany/school/Class.java
@@ -0,0 +1,16 @@
+package com.mycompany.school;
+
+public class Class {
+    private char grade;
+    private char name;
+    private short quantity;
+    private java.util.ArrayList<Pupil> list;
+    
+    Class(char grade, char name, java.util.ArrayList<Pupil> list){
+        this.grade = grade;
+        this.name = name;
+        this.list = list;
+        
+    }
+    
+}
---
+++ School/src/main/java/com/mycompany/school/Pupil.java
@@ -0,0 +1,6 @@
+package com.mycompany.school;
+
+public class Pupil {
+    
+    
+}
